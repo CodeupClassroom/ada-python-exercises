1a1fb353c55bf744d6be8b874d8daf4804e95724
Zach Gulde <zachgulde@gmail.com>
2019-03-06 15:33:33 -0600

    Add import lesson example code

---
+++ imports_lesson.py
@@ -0,0 +1,29 @@
+# import time as t
+
+# print('Starting up...')
+
+# for n in range(10, 0, -1):
+#     print(n)
+#     t.sleep(0.3)
+
+# print('Blast Off!')
+
+# print('All Done!')
+
+# from time import sleep
+
+# for n in range(10):
+#     print(n)
+#     sleep(0.25)
+
+# from time import sleep as wait
+
+# for n in range(10):
+#     print(n)
+#     wait(0.25)
+
+import util
+
+print(sayhello())
+print(sayhowdy())
+
---
+++ util.py
@@ -0,0 +1,21 @@
+SIDES_ON_A_DICE = 6
+
+def sayhowdy():
+    '''
+    A more regionally-efficient version of sayhello
+
+    use this mostly in Texas, not so much elsewhere
+
+    >>> sayhowdy()
+    "Howdy, Y'all"
+
+    Does not accept any arguments
+    '''
+    return 'Howdy, Y\'all!'
+
+def sayhello():
+    'Returns a nice message to the caller.'
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    print('Hello, Ada! Are you excited for early release?')
