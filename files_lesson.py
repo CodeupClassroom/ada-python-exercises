01c8d3c2f300a7e347216a8e9bb8cf1875ceb2dd
Zach Gulde <zachgulde@gmail.com>
2019-03-07 14:27:59 -0600

    Add example of commenting out a given file

--- files_lesson.py
+++ files_lesson.py
@@ -62,3 +62,29 @@ with open('control_structures_exercise.py') as f:
 # with open('example2.txt', 'x') as f:
 #     f.write('Good afternoon, Ada!\n')
 
+# write a program that prompts the user for a python file
+# and comments out all the lines in the file
+from sys import exit
+
+filename = input('Please enter a python filename: ')
+if not filename.endswith('.py'):
+    print('Error: bad filename.')
+    print('       {} does not end with ".py"'.format(filename))
+    exit()
+
+print('Reading file ({}) contents...'.format(filename))
+with open(filename) as f:
+    lines = f.read().split('\n')
+
+# build a new list of commented out code
+commented_out_code = []
+for line in lines:
+    if line.startswith('#') or line == '':
+        commented_out_code.append(line)
+    else:
+        commented_out_code.append('# ' + line)
+
+print('Writing to file: {}'.format(filename))
+
+with open(filename, 'w') as f:
+    f.write('\n'.join(commented_out_code))
