26a20370d355e42d9d42f5653477f2c364241951
Zach Gulde <zachgulde@gmail.com>
2019-03-15 18:51:26 -0500

    Update list comprehension exercise

--- list_comprehension_exercises.py
+++ list_comprehension_exercises.py
@@ -27,11 +27,11 @@ uppercased_fruits = [fruit.upper() for fruit in fruits]
 def count_vowels(word):
     return len([c for c in word.lower() if c in 'aeiou'])
 
-[fruit for fruit in fruits if count_vowels(fruit) > 2]
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if count_vowels(fruit) > 2]
 
 # 4. Make a variable named `fruits_with_only_two_vowels`. The result should be
 #    `['mango', 'kiwi']`
-[fruit for fruit in fruits if count_vowels(fruit) == 2]
+fruits_with_only_two_vowels = [fruit for fruit in fruits if count_vowels(fruit) == 2]
 
 # 5. Make a list that contains each fruit with more than 5 characters
 [fruit for fruit in fruits if len(fruit) > 5]
@@ -48,35 +48,35 @@ def count_vowels(word):
 
 # 9. Make a variable named `fruits_with_letter_a` that contains a list of only the
 #    fruits that contain the letter "a"
-[fruit for fruit in fruits if 'a' in fruit]
+fruits_with_letter_a = [fruit for fruit in fruits if 'a' in fruit]
 
 # 10. Make a variable named `even_numbers` that holds only the even numbers
-[n for n in numbers if n % 2 == 0]
+even_numbers = [n for n in numbers if n % 2 == 0]
 
 # 11. Make a variable named `odd_numbers` that holds only the odd numbers
-[n for n in numbers if n % 2 == 1]
+odd_numbers = [n for n in numbers if n % 2 == 1]
 
 # 12. Make a variable named `positive_numbers` that holds only the positive
 #     numbers
-[n for n in numbers if n > 0]
+positive_numbers = [n for n in numbers if n > 0]
 
 # 13. Make a variable named `negative_numbers` that holds only the negative
 #     numbers
-[n for n in numbers if n < 0]
+negative_numbers = [n for n in numbers if n < 0]
 
 # 14. Use a list comprehension with a conditional in order to produce a list of
 #     numbers with 2 or more numerals
 [n for n in numbers if len(str(n)) >= 2]
 # OR
-[n for n in numbers if n / 10 >= 1]
+[n for n in numbers if n >= 10]
 
 # 15. Make a variable named `numbers_squared` that contains the numbers list with
 #     each element squared. Output is `[4, 9, 16, etc...]`
-[n ** 2 for n in numbers]
+numbers_squared = [n ** 2 for n in numbers]
 
 # 16. Make a variable named `odd_negative_numbers` that contains only the numbers
 #     that are both odd and negative.
-[n for n in numbers if n < 0 and n % 2 == 1]
+odd_negative_numbers = [n for n in numbers if n < 0 and n % 2 == 1]
 
 # 17. Make a variable named primes that is a list containing the prime numbers in
 #     the numbers list.
@@ -89,15 +89,16 @@ def is_prime(n: int) -> bool:
 
     A simple way to check if a number is prime is to say that it is prime if it
     is not evenly divisible by all the whole numbers below it up to the square
-    root of the number. See https://en.wikipedia.org/wiki/Trial_division
+    root of the number[1].
 
     This is a somewhat naive approach and will be very expensive for larger
     numbers.
 
     For more background and more sophisticated approaches, see
-    https://en.wikipedia.org/wiki/Prime_number#Computational_methods
+
+    [1]: https://en.wikipedia.org/wiki/Prime_number#Computational_methods
     '''
-    n = abs(n) # negative numbers
+    n = abs(n) # handle negative numbers
     for x in range(2, math.ceil(math.sqrt(n)) + 1):
         # if it's evenly divisible, it's not a prime number
         if n % x == 0:
@@ -106,4 +107,4 @@ def is_prime(n: int) -> bool:
     # number
     return True
 
-[n for n in numbers if is_prime(n)]
+primes = [n for n in numbers if is_prime(n)]
