0ac70de10b92dc73aae643cbc709108cb5088774
Zach Gulde <zachgulde@gmail.com>
2019-03-15 18:45:42 -0500

    Finish seaborn exercises

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 SIDES_ON_A_DICE = 6
 
+import env
+from sqlalchemy import create_engine
+
 def sayhowdy():
     '''
     A more regionally-efficient version of sayhello
@@ -16,6 +19,13 @@ def sayhowdy():
 def sayhello():
     'Returns a nice message to the caller.'
     return 'Hello, World!'
+
+def get_url_string(user, password, host, db, driver='pymysql'):
+    return f'mysql+{driver}://{user}:{password}@{host}/{db}'
+
+def get_connection(db):
+    url = get_url_string(user=env.user, password=env.password, host=env.host, db=db)
+    return create_engine(url)
 
 if __name__ == '__main__':
     print('Hello, Ada! Are you excited for early release?')
